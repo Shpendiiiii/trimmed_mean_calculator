7aa81e14b3d848e3baf9b0a387fecd0ecb320e70
Shpendiiiii <shpendaliu69@gmail.com>
2023-06-19 22:20:55 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, BufRead};
+use std::fmt::Display;
+fn main() {
+    let mut user_input = Vec::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    loop {
+        let mut input = String::new();
+
+        handle.read_line(&mut input);
+
+        if input.trim().is_empty() {
+            break;
+        }
+
+        user_input.push(input.trim().parse::<i32>().unwrap());
+    }
+    user_input.sort();
+    println!("User input: {:?}", user_input);
+
+    let mut sum: i32 = 0;
+
+    for int in user_input.iter(){
+        sum += int;
+    }
+
+    println!("The sum is: {}", sum)
+
+
+    // stdin.read_line(&mut user_input);
+
+    // println!("Your input: {}", user_input);
+     
+    // for element in user_input.chars(){
+    //     println!("your input {}", element);
+    // }
+}
