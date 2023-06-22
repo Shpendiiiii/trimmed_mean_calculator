7a44cfdd876bfeb1c033f4d15c0c18866c8edb6d
Shpend-dnepSh <116255139+Shpendiiiii@users.noreply.github.com>
2023-06-22 14:58:54 +0200

    fixed some issues with user input handle

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 //work in progress
-
+use std::io::StdinLock;
 use std::io::{self, BufRead};
 use std::fmt::Display;
+use std::process::exit;
+use std::vec;
 fn main() {
     let mut user_input = Vec::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
+    let mut handle = user_input_handle();
 
     loop {
         let mut input = String::new();
 
-        handle.read_line(&mut input);
+        handle.read_line(&mut input).expect("Could not read input. Try again.");
 
         if input.trim().is_empty() {
             break;
@@ -21,27 +22,38 @@ fn main() {
     user_input.sort_by(|a, b| a.partial_cmp(b).unwrap());
     println!("User input: {:?}", user_input);
 
-    let vecLen = user_input.len();
+    let vec_len: usize = user_input.len();
 
-    let mut sum: i8 = 0;
-
-    let mut removeNValues = String::new();
-    
-    handle.read_line(&mut removeNValues).expect("Failed to read value, try again");
-
-    let nValue = match removeNValues.trim().parse::<i8>() {
-        Ok(i) => println!("{} elements will be taken out", i),
-        Err(..) => println!("The input was not an int")
-    };
-
-   
+    let mut sum: i64 = 0;
 
 
-    // stdin.read_line(&mut user_input);
+    let n_count = remove_n_values(&mut handle);
 
-    // println!("Your input: {}", user_input);
-     
-    // for element in user_input.chars(){
-    //     println!("your input {}", element);
-    // }
+
+    if n_count > ((vec_len / 2).try_into().unwrap()){
+        print!("Not possible");
+        // exit(0)
+    } else { println!("Possible") }
+
+}
+
+fn remove_n_values(handle: &mut StdinLock<'static>) -> i64 {
+    let mut remove_n_values = String::new();
+
+    handle.read_line(&mut remove_n_values).expect("Failed to read value, try again");
+
+
+    let mut n_count: i64 = 0;
+
+    match remove_n_values.trim().parse::<i64>() {
+        Ok(i) => n_count = i,
+        Err(..) => println!("Parse Int Error")
+    }
+    n_count
+}
+
+fn user_input_handle() -> StdinLock<'static> {
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    handle
 }
