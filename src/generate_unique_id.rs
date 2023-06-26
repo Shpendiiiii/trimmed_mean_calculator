72c54678d589a8806fa607bcb8870cdb18cdf4d6
Shpend-dnepSh <116255139+Shpendiiiii@users.noreply.github.com>
2023-06-26 21:49:33 +0200

    fixes within generate_unique_id.rs

---
+++ src/generate_unique_id.rs
@@ -0,0 +1,11 @@
+
+
+use std::time::SystemTime;
+
+pub fn generate_unique_id() -> String {
+    let timestamp = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .expect("Error")
+        .as_secs();
+    format!("data/output_{}", timestamp)
+}
